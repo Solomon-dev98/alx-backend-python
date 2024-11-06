0df9760a13dca9a0af7104f09c5eb932c9619cd3
Solomon-dev98 <asapnino01@gmail.com>
2024-11-06 12:53:21 +0100

    Feat(test_utils.py): Add parametrizing a unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+#!/user/bin/env python3
+"""Parameterizing a unit test"""
+import unittest
+import requests
+from unittest import mock
+from unittest.mock import patch
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """A class to test utils.access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self):
+        """Test access_nested_map function"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
